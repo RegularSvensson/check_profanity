38ba8a4b59ce20d04b8a70c2ccd49576cedf55c3
RegularSvensson <elias.svensson.1992@gmail.com>
2015-04-11 14:29:28 +0200

    Add function to Check profanity

--- check_profanity.py
+++ check_profanity.py
@@ -6,4 +6,11 @@ def read_text():
 	contents_of_file = quotes.read()
 	print(contents_of_file)
 	quotes.close()
+#Check profanity
+def check_profanity(text_to_check):
+	connection = urllib.urlopen("http://www.wdyl.com/profanity?q=" + text_to_check)
+	output = connection.read()
+	print(output)
+	connection.close()
+
 read_text()
