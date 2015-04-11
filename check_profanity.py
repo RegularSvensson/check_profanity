b2022cc28547dbb1ecac66a8cbe24b59a8042594
RegularSvensson <elias.svensson.1992@gmail.com>
2015-04-11 14:20:12 +0200

    Add function to read text from document & call function

--- check_profanity.py
+++ check_profanity.py
@@ -1,1 +1,7 @@
-check_profanity.py
+#Read text from document
+def read_text():
+	quotes = open("/Users/eliasvensson/Udacity/full-stack/PFWP/check_profanity/movie_quotes.txt")
+	contents_of_file = quotes.read()
+	print(contents_of_file)
+	quotes.close()
+read_text()
